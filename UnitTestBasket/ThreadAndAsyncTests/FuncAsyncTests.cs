8ed54e9d5ec3f45ab7aabded5d039dda9c9570aa
Jeffrey Su <zsu@senparc.com>
2019-04-21 22:31:57 +0800

    初始化项目

---
+++ UnitTestBasket/ThreadAndAsyncTests/FuncAsyncTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ThreadAndAsyncTests
+{
+    [TestClass]
+    public class FuncAsyncTests
+    {
+        /// <summary>
+        /// 测试 Func 异步方法的执行顺序
+        /// </summary>
+        [TestMethod]
+        public void FuncAsyncTest()
+        {
+
+        }
+    }
+}
